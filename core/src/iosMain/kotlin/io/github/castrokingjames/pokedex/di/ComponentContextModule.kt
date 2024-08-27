c7d4df4332d600f1e485181b26859b62fc538569
King James Castro <castro.kingjames@gmail.com>
2024-08-27 15:37:04 +0800

    Removed iOS target

--- core/src/iosMain/kotlin/io/github/castrokingjames/pokedex/di/ComponentContextModule.kt
+++
@@ -1,7 +0,0 @@
-package io.github.castrokingjames.pokedex.di
-
-import org.koin.dsl.module
-
-actual val componentContextModule = module {
-
-}
--- core/src/iosMain/kotlin/io/github/castrokingjames/pokedex/di/DispatchersModule.kt
+++
@@ -1,5 +0,0 @@
-package io.github.castrokingjames.pokedex.di
-
-import org.koin.dsl.module
-
-actual val dispatchersModule = module {}
